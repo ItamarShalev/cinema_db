0ebfe5a92ee7f545ee564d53eae7f75bb205a737
Itamar Shalev <itamar1111111@gmail.com>
2023-03-03 06:57:20 +0200

    tests: Add C# framework for tests execution.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

---
+++ cinema_db/UnitTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cinema_db
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestUnitTest()
+        {
+           Assert.AreEqual(1, 1);
+        }
+    }
+}
