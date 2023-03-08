99bc485b5fa6dfa8cbd9a881e27875da45ade207
Itamar Shalev <itamar1111111@gmail.com>
2023-03-08 20:39:21 +0200

    sql: data: scheme: Make every row unique.
    Signed-off-by: Itamar Shalev <itamar1111111@gmail.com>

--- sql/data/scheme.sql
+++ sql/data/scheme.sql
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS employee
     first_name    VARCHAR(50) NOT NULL,
     last_name     VARCHAR(50) NOT NULL,
     date_of_birth DATE        NOT NULL,
+    UNIQUE (first_name, last_name, date_of_birth),
     PRIMARY KEY (id)
 );
 
@@ -20,6 +21,7 @@ CREATE TABLE IF NOT EXISTS product
     id           INT         NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(50) NOT NULL,
     price        INT         NOT NULL,
+    UNIQUE (product_name, price),
     PRIMARY KEY (id),
     CHECK (price > 0)
 );
@@ -30,6 +32,7 @@ CREATE TABLE IF NOT EXISTS movie
     movie_name          VARCHAR(50) NOT NULL,
     rating              VARCHAR(50) NOT NULL,
     duration_in_minutes INT         NOT NULL,
+    UNIQUE (movie_name, rating, duration_in_minutes),
     PRIMARY KEY (id),
     CHECK (duration_in_minutes > 0 AND rating IN ('G', 'PG', 'PG-13', 'R'))
 );
@@ -40,6 +43,7 @@ CREATE TABLE IF NOT EXISTS theater
     is_3d       TINYINT NOT NULL DEFAULT 0,
     seats       INT     NOT NULL,
     is_vip      TINYINT NOT NULL DEFAULT 0,
+    UNIQUE (room_number, is_3d, seats, is_vip),
     PRIMARY KEY (room_number),
     CHECK (seats > 0 AND is_3d IN (0, 1) AND is_vip IN (0, 1))
 );
@@ -49,6 +53,7 @@ CREATE TABLE IF NOT EXISTS customer
     id            INT          NOT NULL AUTO_INCREMENT,
     customer_name VARCHAR(100) NOT NULL,
     date_of_birth DATE         NOT NULL,
+    UNIQUE (customer_name, date_of_birth),
     PRIMARY KEY (id)
 );
 
@@ -57,6 +62,7 @@ CREATE TABLE IF NOT EXISTS department
     id              INT          NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(100) NOT NULL,
     manager_id      INT          NOT NULL,
+    UNIQUE (department_name, manager_id),
     PRIMARY KEY (id),
     INDEX idx_fk_department (manager_id),
     CONSTRAINT fk_department FOREIGN KEY (manager_id) REFERENCES employee (id)
@@ -70,6 +76,7 @@ CREATE TABLE IF NOT EXISTS food
     need_cooling TINYINT NOT NULL DEFAULT 0,
     allergy      VARCHAR(10),
     min_age      INT     NOT NULL,
+    UNIQUE (need_cooling, allergy, min_age),
     CHECK ( need_cooling IN (0, 1) AND min_age > 0 AND allergy IN ('nuts', 'milk', 'soy')),
     INDEX idx_fk_food (id),
     CONSTRAINT fk_food FOREIGN KEY (id) REFERENCES product (id)
@@ -82,6 +89,7 @@ CREATE TABLE IF NOT EXISTS screen
     screen_time DATETIME NOT NULL,
     room_number INT      NOT NULL,
     movie_id    INT      NOT NULL,
+    UNIQUE (screen_time, room_number, movie_id),
     PRIMARY KEY (screen_time, room_number),
     INDEX idx_fk_screen_room (room_number),
     CONSTRAINT fk_screen_room FOREIGN KEY (room_number) REFERENCES theater (room_number)
@@ -99,6 +107,7 @@ CREATE TABLE IF NOT EXISTS sell
     customer_id INT      NOT NULL,
     product_id  INT      NOT NULL,
     sell_time   DATETIME NOT NULL,
+    UNIQUE (employee_id, customer_id, product_id, sell_time),
     PRIMARY KEY (employee_id, customer_id, product_id, sell_time),
     INDEX idx_fk_sell_employee (employee_id),
     CONSTRAINT fk_sell_employee FOREIGN KEY (employee_id) REFERENCES employee (id)
@@ -125,6 +134,7 @@ CREATE TABLE IF NOT EXISTS ticket
     screen_time DATETIME NOT NULL,
     room_number INT      NOT NULL,
     screen_id   INT      NOT NULL,
+    UNIQUE (seat_number, screen_time, room_number, screen_id),
     PRIMARY KEY (screen_id, seat_number),
     INDEX idx_fk_ticket (id),
     CONSTRAINT fk_ticket FOREIGN KEY (id) REFERENCES product (id)
